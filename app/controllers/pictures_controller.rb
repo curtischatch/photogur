7550cf7e38f65ef5419e7da165305e8fe3ab4be8
curtischatch <curtis.chat.hk@gmail.com>
2018-08-11 16:48:47 -0400

    revised index

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -19,32 +19,35 @@ class PicturesController < ApplicationController
     @picture.artist = params[:picture][:artist]
     @picture.url = params[:picture][:url]
 
-
     if @picture.save
-      # if the picture gets saved, generate a get request to "/pictures" (the index)
       redirect_to "/pictures"
     else
-      # otherwise render new.html.erb
       render :new
     end
   end
 
   def edit
-      @picture = Picture.find(params[:id])
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+
+    @picture.title = params[:picture][:title]
+    @picture.artist = params[:picture][:artist]
+    @picture.url = params[:picture][:url]
+
+    if @picture.save
+      redirect_to "/pictures/#{@picture.id}"
+    else
+      render :edit
     end
+  end
 
-    def update
-      @picture = Picture.find(params[:id])
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to "/pictures"
+  end
 
-      @picture.title = params[:picture][:title]
-      @picture.artist = params[:picture][:artist]
-      @picture.url = params[:picture][:url]
-
-
-      if @picture.save
-        redirect_to "/pictures/#{@picture.id}"
-      else
-        render :edit
-      end
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   post 'pictures' => 'pictures#create'
   get 'pictures/new' => 'pictures#new'
 
-  get 'pictures/:id' => 'pictures#show'
+  get 'pictures/:id/edit' => 'pictures#edit'
+  patch 'pictures/:id/' => 'pictures#update'
 
+  delete 'pictures/:id' => 'pictures#destroy'
+
+  get 'pictures/:id' => 'pictures#show'
 end
