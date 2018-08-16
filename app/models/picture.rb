09237273faf98688bd500b5c92ee9c14ed9dd1e3
curtischatch <curtis.chat.hk@gmail.com>
2018-08-16 14:08:44 -0400

    debug

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,22 +1,14 @@
 class Picture < ApplicationRecord
+  validates :title, length: {within: 3..20}
   validates :artist, :url, presence: true
-  validates :title, length: {minimum: 3, maximum: 20}
-  validates :url, uniqueness: true
+  validates :url,
+            uniqueness: true
 
+  def self.created_before(time)
+    Picture.where("created_at < ?", time)
+  end
 
-def self.newest_first
-  Picture.order("created_at DESC")
-end
-
-def self.most_recent_five
-  Picture.newest_first.limit(5)
-end
-
-def self.created_before(time)
-  Picture.where("created_at < ?", time)
-end
-def self.created_in_year(year)
-  Picture.where("created_at LIKE ?", "#{year}%")
-end
-
+  def self.created_in_year(year)
+    Picture.where("created_at LIKE ?", "#{year}%")
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180808021659) do
+ActiveRecord::Schema.define(version: 20180805030732) do
 
   create_table "pictures", force: :cascade do |t|
     t.string   "artist"
