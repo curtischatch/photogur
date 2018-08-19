8684652762c30a26aca49975da0b08b808adcc09
curtischatch <curtis.chat.hk@gmail.com>
2018-08-19 15:04:08 -0400

    error when loggin in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,9 +10,11 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to pictures_url, notice: "Logged in!"
     else
+      flash[:notice] = 'try again'
       render :new
     end
   end
+
 
   def destroy
     sessions[:user_id] = nil
